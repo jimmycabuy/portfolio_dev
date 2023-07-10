0c13bb6f08b26e3f202f06909c7567e4824d8d3d
Jimmy Cabuy <jimmy.cabuy@decathlon.com>
2023-07-10 09:28:37 +0200

    clean assets

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -2,12 +2,12 @@
   <img src="/assets/html.png" alt="html" />
   <img src="/assets/css.png" alt="css" />
   <img src="/assets/javascript.png" alt="javascript" />
-  <img src="/assets/react2.png" alt="react" />
+  <img src="/assets/react.png" alt="react" />
   <img src="/assets/redux.png" alt="redux" />
-  <img src="/assets/svelte2.png" alt="svelte" />
+  <img src="/assets/svelte.png" alt="svelte" />
   <img src="/assets/tailwind.png" alt="tailwind" />
   <img src="/assets/nodejs.png" alt="nodejs" />
-  <img src="/assets/mongodb2.png" alt="mongodb" />
+  <img src="/assets/mongodb.png" alt="mongodb" />
   <img src="/assets/git.png" alt="git" />
   <img src="/assets/figma.png" alt="figma" />
   <img src="/assets/photoshop.png" alt="photoshop" />
