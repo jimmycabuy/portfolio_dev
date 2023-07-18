a0f77f9f405c93f867c7d4aa71835f02f26f3d1b
Jimmy Cabuy <jimmy.cabuy@decathlon.com>
2023-07-18 18:54:29 +0200

    add +error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <h1>{$page.status}</h1>
+  <h1>Oops! Page not found</h1>
+  <h4>Sorry, the page you are looking for does not exist.</h4>
+  <a href="/">Homepage</a>
+</main>
+
+<style>
+  main {
+    height: 70vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+  }
+  a {
+    margin-top: 1rem;
+  }
+
+  a {
+    position: relative;
+    text-decoration: none;
+    color: #a1a1a1;
+    width: fit-content;
+  }
+
+  @media (min-width: 1023px) {
+    main {
+      height: 80vh;
+    }
+    a::after {
+      content: "";
+      position: absolute;
+      bottom: -2px;
+      left: 0;
+      width: 0;
+      height: 2px;
+      background-color: #a1a1a1;
+      transition: width 0.3s ease-in-out;
+    }
+
+    a:hover::after {
+      width: 100%;
+    }
+  }
+</style>
