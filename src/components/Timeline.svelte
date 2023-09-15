f04c61cb44f1dfe42e1338bf351aeb70c71fa626
Jimmy Cabuy <jimmy.cabuy@decathlon.com>
2023-09-16 00:29:12 +0200

    timeline

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,60 +1,81 @@
 <script>
   import timeline from "../data/timeline.json";
-
-  const startYear = 2015;
-  const nextYear = new Date().getFullYear() + 1;
-  const numberOfYear = nextYear - startYear;
 </script>
 
 <main class="px-10 xl:px-36 py-6 w-full">
   <h1 class="my-10 text-5xl md:text-7xl">Timeline</h1>
-  <div
-    class="grid_container"
-    style="grid-template-columns: repeat({numberOfYear}, 1fr); grid-template-rows: repeat({timeline.length}, 1fr);"
-  >
+  <ol class="timeline">
     {#each timeline as item}
-      <div
-        class="one_element"
-        style="grid-area: {item.startRow} / {item.startCol} / {item.endRow} / {item.endCol};"
-      >
-        <h2>{item.year}</h2>
-        <h2>{item.title}</h2>
-      </div>
+      <li class="timeline-item">
+        <span class="timeline-item-icon | filled-icon">
+          <svg
+            width="24"
+            height="24"
+            viewBox={item?.viewbox}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="#ff2f66" d={item?.d} />
+          </svg>
+        </span>
+        <div class="timeline-item-wrapper">
+          <div class="timeline-item-description">
+            <span>{item?.year} > {item?.title}</span>
+          </div>
+          <div class="comment">
+            <p class="md:text-justify">
+              {item?.description}
+            </p>
+          </div>
+        </div>
+      </li>
     {/each}
-  </div>
+  </ol>
 </main>
 
 <style>
-  @media (min-width: 1023px) {
-    .grid_container {
-      display: grid;
-      grid-row-gap: 1.5rem;
-      width: 100%;
-    }
+  .timeline {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 32px 0 32px 32px;
+    border-left: 2px solid #fff;
+    gap: 2rem;
+  }
+  .timeline-item {
+    display: flex;
+    gap: 24px;
   }
 
-  @media (max-width: 1022px) {
-    .grid_container {
-      width: 100%;
-      display: flex;
-      gap: 1.5rem;
-      flex-direction: column;
-    }
+  .timeline-item-icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: -52px;
+    flex-shrink: 0;
+    overflow: hidden;
+  }
+  .timeline-item-icon svg {
+    width: 20px;
+    height: 20px;
+  }
+  .timeline-item-icon.filled-icon {
+    background-color: #fff;
+  }
+  .timeline-item-description {
+    display: flex;
+    padding-top: 6px;
   }
 
+  .comment {
+    margin-top: 12px;
+    border-radius: 6px;
+  }
   h1 {
     color: transparent;
     -webkit-text-stroke: 2px #fff;
     font-weight: 700;
-  }
-
-  .one_element {
-    border: 2px solid #fff;
-    border-radius: 3rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 5rem;
-    flex-direction: column;
   }
 </style>
