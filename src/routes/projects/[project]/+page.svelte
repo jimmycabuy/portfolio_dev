ae5ab6bca9209819ca7bf2e046ff7f1ea688c731
Jimmy Cabuy <jimmy.cabuy@decathlon.com>
2023-10-24 16:48:41 +0200

    show link or not

--- src/routes/projects/[project]/+page.svelte
+++ src/routes/projects/[project]/+page.svelte
@@ -87,26 +87,28 @@
 
     <div class="bloc_right">
       <p>{subProjectDetails.description}</p>
-      <a href={subProjectDetails.href} target="_blank">
-        <span class="view_website">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="3 0 24 20"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-5 h-5"
-            color="#fff"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-            />
-          </svg>
-          <span>View {subProjectDetails.type}</span>
-        </span>
-      </a>
+      {#if subProjectDetails.showlink}
+        <a href={subProjectDetails.href} target="_blank">
+          <span class="view_website">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="3 0 24 20"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+              color="#fff"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+            <span>View {subProjectDetails.type}</span>
+          </span>
+        </a>
+      {/if}
     </div>
   </div>
 
