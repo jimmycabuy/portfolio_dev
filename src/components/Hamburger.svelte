7026f72be018fe57d1ddfae5577882ca9d5914cf
Jimmy Cabuy <jimmy.cabuy@decathlon.com>
2024-02-24 23:14:59 +0100

    refacto

--- src/components/Hamburger.svelte
+++ src/components/Hamburger.svelte
@@ -2,18 +2,8 @@
   export let open = false;
 </script>
 
-<button
-  class="border-none focus:outline-none"
-  class:open
-  on:click={() => (open = !open)}
->
-  <h2 class="enlarged text-2xl sm:text-3xl">
-    {#if !open}
-      MENU.
-    {:else}
-      CLOSE.
-    {/if}
-  </h2>
+<button class="border-none focus:outline-none" on:click={() => (open = !open)}>
+  <h2 class="enlarged text-2xl sm:text-3xl">{open ? "CLOSE." : "MENU."}</h2>
 </button>
 
 <style>
